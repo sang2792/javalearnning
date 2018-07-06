90459fbb32366f7659abab9b0e7f0fe0cc8df69f
sang2792 <tranhuusang2792@gmail.com>
2018-07-05 18:58:07 -0700

    hello world

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
 public class helloworld {
+	public void  Chao(){
+		system.out.println("xin chao the gioi");
+	}		
+	
 	public static void main (Strinf args[]){
+		Chao();
 		system.out.println("hello world");
 	}
 
